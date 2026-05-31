0e99c6aebb5160d49702257a46fe922a275bb9af
Jasmin Hoffmann <jasminhoffmann436@gmail.com>
2026-05-31 18:56:58 +0200

    collision for gridtile works now again

--- roguelike-farming/Scripts/DungeonThings/DungeonBuilder/placeable_tile_2.gd
+++ roguelike-farming/Scripts/DungeonThings/DungeonBuilder/placeable_tile_2.gd
@@ -33,9 +33,23 @@ func _on_button_up() -> void:
 	is_dragging = false
 	var back_tween = create_tween()
 	back_tween.tween_property(self, "position", snapback_pos, 0.1)
+	getGridTile()
 	# then also checking if the tile should be placed somewhere
 
 func _on_gui_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.double_click:
 			tile_visualisation.rotateTile()
+
+func getGridTile() -> void: 
+	var mouse_pos = get_viewport().get_mouse_position()
+	var space = get_world_2d().direct_space_state
+	var query = PhysicsPointQueryParameters2D.new()
+	query.position = mouse_pos
+	query.collide_with_areas = true
+	var everything = space.intersect_point(query)
+	for slot in space.intersect_point(query):
+		var s: GridTile = slot.collider.get_parent() as GridTile
+		if s != null:
+			#logic to place the specific tile
+			print("Found free grid Space")
